c821b18bf5ae608a9a2857cd05b82f61e36f0c84
Temitope Olotin <olotintemitope@gmail.com>
2022-07-13 21:40:12 +0100

    push file up

---
+++ src/Providers/TranslationServiceProvider.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Lt\LaraVueTranslate\Providers;
+
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\ServiceProvider;
+
+class TranslationServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Cache::rememberForever('translations', function () {
+            $translations = collect();
+            $locales = array_map(
+                function($dir) {
+                    return basename($dir);
+                }, glob('../../resources/lang/*')
+            );
+
+            foreach ($locales as $locale) {
+                $translations[$locale] = [
+                    'php' => $this->phpTranslations($locale),
+                    'json' => $this->jsonTranslations($locale),
+                ];
+            }
+
+            return $translations;
+        });
+
+        $this->publishes([
+            __DIR__.'/../../resources/assets' => base_path('resources/assets'),
+        ]);
+    }
+
+    private function phpTranslations($locale)
+    {
+        $path = resource_path("lang/$locale");
+
+        return collect(File::allFiles($path))->flatMap(function ($file) use ($locale) {
+            $key = ($translation = $file->getBasename('.php'));
+
+            return [$key => trans($translation, [], $locale)];
+        });
+    }
+
+    private function jsonTranslations($locale)
+    {
+        $path = resource_path("lang/$locale.json");
+
+        if (is_string($path) && is_readable($path)) {
+            return json_decode(file_get_contents($path), true);
+        }
+
+        return [];
+    }
+}
